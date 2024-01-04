9231b4b911396629c1355960262ded3e8168218f
bouzenaali <Alibouzena16@gmail.com>
2024-01-04 22:41:36 +0100

    feat: add lawyer signup urls

--- Backend/accounts/urls.py
+++ Backend/accounts/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
 from . import views
+from .views import LawyerSignupView
 
 app_name = 'accounts'
 
 urlpatterns = [
     path('', views.home, name='home'),
     path('logout/', views.logout, name='logout'),
+    path('signup/lawyer/', LawyerSignupView.as_view(), name='lawyer_signup'),
 ]
