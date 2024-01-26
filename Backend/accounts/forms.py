ff3f62307935fd600875a71a06400c8de5807017
bouzenaali <Alibouzena16@gmail.com>
2024-01-26 11:46:23 +0100

    feat: add the form and the serializer for the lawyersignup view

--- Backend/accounts/forms.py
+++ Backend/accounts/forms.py
@@ -4,27 +4,31 @@ from .models import Lawyer, Wilaya, Commune, Category, Address
 
 
 class LawyerSignupForm(SignupForm):
+
     wilaya = forms.CharField(max_length=30, label='wilaya')
     commune = forms.CharField(max_length=30, label='commune')
-    Address = forms.CharField(max_length=30, label='address')
+    Address = forms.TextField(label='address')
     experience = forms.DecimalField(max_length=30, label='experience')
-    category = forms.CharField(max_length=500, label='category')
-    description = forms.TextField(max_length=30, label='description')
+    categories = forms.TextField(label='category')
+    description = forms.TextField(label='description')
+    link_to_personal_website = forms.URLField(required=False, label='link_to_personal_website')
 
     def save(self, request):
         user = super(LawyerSignupForm, self).save(request)
+        email = self.cleaned_data.get('email')
         wilaya = Wilaya.objects.get(name=self.cleaned_data.get('wilaya'))
         commune = Commune.objects.get(name=self.cleaned_data.get('commune'))
         address = Address.objects.get(name=self.cleaned_data.get('address'))
-        category = Category.objects.get(name=self.cleaned_data.get('category'))
+        categories = Category.objects.get(name=self.cleaned_data.get('category'))
         experience = self.cleaned_data.get('experience')
         description = self.cleaned_data.get('description')
         lawyer = Lawyer.objects.create(
             user=user,
+            email=email,
             wilaya=wilaya,
             commune=commune,
             address=address,
-            category=category,
+            category=categories,
             experience=experience,
             description=description
         )
--- Backend/accounts/serializers.py
+++ Backend/accounts/serializers.py
@@ -1,34 +1,43 @@
 from rest_framework import serializers
-from django.contrib.auth.models import User
 from objects.models import Wilaya, Commune, Address, Category
-from accounts.models import Lawyer
+from accounts.models import Lawyer, CustomUser
+
+
+class CustomUserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = CustomUser
+        fields = ('username', 'password')
+        extra_kwargs = {'password': {'write_only': True}}
 
 class LawyerSignupSerializer(serializers.ModelSerializer):
+    user = CustomUserSerializer()
     fullname = serializers.CharField(max_length=50)
+    email = serializers.CharField(max_length=50)
     wilaya = serializers.PrimaryKeyRelatedField(queryset=Wilaya.objects.all())
     commune = serializers.PrimaryKeyRelatedField(queryset=Commune.objects.all())
-    Address = serializers.CharField(max_length=30)
+    Address = serializers.CharField(max_length=200)
     experience = serializers.DecimalField(max_digits=5, decimal_places=2)
-    category = serializers.CharField(max_length=30)
+    categories = serializers.CharField(style={'base_template': 'textarea.html'})
     description = serializers.CharField(style={'base_template': 'textarea.html'})
     link_to_personal_website = serializers.URLField(required=False)
 
     class Meta:
-        model = User
-        fields = ('fullname', 'username', 'password', 'wilaya', 'commune', 'Address', 'experience', 'category', 'description', 'link_to_personal_website')
+        model = Lawyer
+        fields = ('user', 'fullname', 'email', 'wilaya', 'commune', 'Address', 'experience', 'categories', 'description', 'link_to_personal_website')
         extra_kwargs = {'password': {'write_only': True}}
 
     def create(self, validated_data):
-        user = User.objects.create_user(
-            validated_data['username'],
-            password=validated_data['password']
-        )
-        address = Address.objects.get(id=validated_data['address'])
-        categories = Category.objects.filter(id__in=validated_data['categories'])
+        user_data = validated_data.pop('user')
+        custom_user = CustomUser.objects.create_user(**user_data)
+        address_data = validated_data.pop('address')
+        address = Address.objects.create(**address_data)
+        categories = validated_data['categories']
+        email = validated_data['email']
         wilaya = validated_data['wilaya']
         commune = validated_data['commune']
         lawyer = Lawyer.objects.create(
-            user=user,
+            user=custom_user,
+            email=email,
             address=address,
             wilaya=wilaya,
             commune=commune,
